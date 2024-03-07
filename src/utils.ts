7617991fbff29a74c2540830975d5495a46e03d8
Robson Xavier <rbsmail93@gmail.com>
2024-03-06 23:32:04 -0300

    Fix regex on config file

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,28 @@
 export type Config = {
-  regexCommitMessage: RegExp;
-  regexBranchName: RegExp;
-  branchErrorMessage: string;
-  commitErrorMessage: string;
+  branch: {
+    branchErrorMessage: string;
+    regexBranchName: RegExp;
+    ignore: string[];
+  };
+  commit: {
+    commitErrorMessage: string;
+    regexCommitMessage: RegExp;
+  };
 };
 export const CONFIG_FILE_NAME = ".genforce.config.json";
 
+Object.defineProperty(RegExp.prototype, "toJSON", {
+  value: RegExp.prototype.toString,
+});
+
 export const DEFAULT_CONFIG_FILE: Config = {
-  regexBranchName: /feature/,
-  branchErrorMessage: "default error message",
-  regexCommitMessage: /feat:*/,
-  commitErrorMessage: "default error message",
+  branch: {
+    regexBranchName: /feature/,
+    branchErrorMessage: "default error message",
+    ignore: [],
+  },
+  commit: {
+    regexCommitMessage: /feat:*/,
+    commitErrorMessage: "default error message",
+  },
 };
